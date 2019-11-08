1cca3cb48060d318622b61cf48c6a35d207b915f
Alexander Wilts <alexander.wilts@codecentric.de>
2019-11-08 22:10:32 +0100

    added typescript to further files

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-        {/*<AddMessage/>*/}
+        <AddMessage/>
         <ListMessages/>
       </header>
     </div>
--- src/components/AddMessage.tsx
+++ src/components/AddMessage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function AddMessage(props) {
+function AddMessage() {
     return (
         <div>
             <form>
