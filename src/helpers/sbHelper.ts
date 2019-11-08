b279d82b4035419ddff1280c1f1e578d3a9bfff6
Alexander Wilts <alexander.wilts@codecentric.de>
2019-11-08 21:00:57 +0100

    extended typescript to sbHelper

--- src/helpers/sbHelper.ts
+++ src/helpers/sbHelper.ts
@@ -1,9 +1,14 @@
 import config from "config";
 import {ServiceBusManagementClient} from "@azure/arm-servicebus";
 import {ServiceBusClient} from "@azure/service-bus";
+import {DeviceTokenCredentials} from "@azure/ms-rest-nodeauth";
 
-export async function listNamespaces(creds) {
-    const subscriptionId = config.get('azure.subscriptionId');
+const subscriptionId: string = config.get('azure.subscriptionId');
+const resourceGroup: string = config.get('azure.resourceGroup');
+const namespace: string = config.get('azure.namespace');
+const authorizationRuleName: string = config.get('serviceBus.userName');
+
+export async function listNamespaces(creds: DeviceTokenCredentials) {
     const client = new ServiceBusManagementClient(creds, subscriptionId);
     client.namespaces.list().then(namespaces => {
         namespaces.map(ns => {
@@ -12,24 +17,17 @@ export async function listNamespaces(creds) {
     });
 }
 
-export async function readFirstMessage(creds) {
-    const subscriptionId = config.get('azure.subscriptionId');
-    const resourceGroup = config.get('azure.resourceGroup');
-    const namespace = config.get('azure.namespace');
-    const authorizationRuleName = config.get('serviceBus.userName');
+export async function readFirstMessage(creds: DeviceTokenCredentials) {
     const client = new ServiceBusManagementClient(creds, subscriptionId);
     const namespacesListKeys = await client.namespaces.listKeys(resourceGroup, namespace, authorizationRuleName);
     const connectionString = namespacesListKeys.primaryConnectionString;
-    const serviceBusClient = ServiceBusClient.createFromConnectionString(connectionString);
+    const serviceBusClient = ServiceBusClient.createFromConnectionString(connectionString!);
     const queueClient = serviceBusClient.createQueueClient("sandbox");
     const peekedMessage = await queueClient.peek();
     return peekedMessage[0];
 }
 
-export async function getQueueDetails(creds, queueName) {
-    const resourceGroup = config.get('azure.resourceGroup');
-    const namespace = config.get('azure.namespace');
-    const subscriptionId = config.get('azure.subscriptionId');
+export async function getQueueDetails(creds: DeviceTokenCredentials, queueName: string) {
     const client = new ServiceBusManagementClient(creds, subscriptionId);
     const queues = await client.queues.listByNamespace(resourceGroup, namespace);
     const goodQueue = queues.filter(queue => queue.name === queueName);
